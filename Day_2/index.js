6d3fb1f215b9aa391395238ce22192ee87ac5f58
chocoboy2004 <iamrahulghoshinfo@gmail.com>
2024-10-03 18:45:51 +0530

    Creating a server using express & discussing about HTTP methods

---
+++ Day_2/index.js
@@ -0,0 +1,52 @@
+import express from "express"
+
+const app = express()
+const port = 3000
+
+// If you write anything after forward slash (/), It will be considered as an end-point.
+
+app.get("/", (req, res) => {
+    res.send("Hello World!")
+})
+
+app.get("/login", (req, res) => {
+    res.send("It is a login page! Please login !!")
+})
+
+app.get("/logout", (req, res) => {
+    res.send("It is a logout page!")
+})
+
+app.get("/greeting", (req, res) => {
+    res.send("Welcome!")
+})
+
+app.listen(port, () => {
+    console.log("Server is running !!!")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+    HTTP Methods (HyperText Transfer Protocol)
+    POST, GET, DELETE, PATCH, PUT
+
+    POST: Help us to send data to the server securely.
+    GET: Retreive data from server.
+    DELETE: Help us to delete any particular data from the database.
+    PATCH: Updating a particular thing.
+    PUT: Updating The whole data.
+*/  
